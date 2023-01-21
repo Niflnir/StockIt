93146eb1ccff395494f497a58169f3a69fd5a1e9
Niflnir <shaunchenggy@gmail.com>
2023-01-21 20:38:26 +0800

    added login and signup routes

---
+++ server/src/routes/auth/login.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../../models/user";
+import { Password } from "../../services/password";
+import jwt from "jsonwebtoken";
+import { BadRequestError, validateRequest } from "../../utils/utils";
+
+const router = express.Router();
+
+router.post(
+  "/api/auth/login",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Check if passwords match
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
+
+export { router as loginRouter };
