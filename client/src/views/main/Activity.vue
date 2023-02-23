98f2703b2f72764ced00cf603d50594f8cac94bd
Chris Tang <christang_1023@outlook.com>
2023-02-23 12:01:05 +0800

    Add card to ActivityLog page

--- client/src/views/main/Activity.vue
+++ client/src/views/main/Activity.vue
@@ -1,6 +1,47 @@
-<template lang="">
+<template lang="html">
   <div>
-    Activity Log
+    <iq-card headerClass="flex-wrap" data-aos="fade-up" data-aos-delay="400">
+      <template v-slot:headerTitle>
+        <h4 class="card-title mb-2">Activity overview</h4>
+      </template>
+      <template v-slot:body>
+        <div class=" d-flex profile-media align-items-top mb-2">
+          <div class="profile-dots-pills border-primary mt-1"></div>
+          <div class="ms-4">
+            <h6 class=" mb-1">$2400, Purchase</h6>
+            <span class="mb-0">11 JUL 8:10 PM</span>
+          </div>
+        </div>
+        <div class=" d-flex profile-media align-items-top mb-2">
+          <div class="profile-dots-pills border-primary mt-1"></div>
+          <div class="ms-4">
+            <h6 class=" mb-1">New order #8744152</h6>
+            <span class="mb-0">11 JUL 11 PM</span>
+          </div>
+        </div>
+        <div class=" d-flex profile-media align-items-top mb-2">
+          <div class="profile-dots-pills border-primary mt-1"></div>
+          <div class="ms-4">
+            <h6 class=" mb-1">Affiliate Payout</h6>
+            <span class="mb-0">11 JUL 7:64 PM</span>
+          </div>
+        </div>
+        <div class=" d-flex profile-media align-items-top mb-2">
+          <div class="profile-dots-pills border-primary mt-1"></div>
+          <div class="ms-4">
+            <h6 class=" mb-1">New user added</h6>
+            <span class="mb-0">11 JUL 1:21 AM</span>
+          </div>
+        </div>
+        <div class=" d-flex profile-media align-items-top mb-1">
+          <div class="profile-dots-pills border-primary mt-1"></div>
+          <div class="ms-4">
+            <h6 class=" mb-1">Product added</h6>
+            <span class="mb-0">11 JUL 4:50 AM</span>
+          </div>
+        </div>
+      </template>
+    </iq-card>
   </div>
 </template>
 <script setup>
