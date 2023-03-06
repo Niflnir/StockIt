642fc94e57d52d964169c29597c2ba187bd8dfee
Chris Tang <christang_1023@outlook.com>
2023-03-06 15:40:59 +0800

    Integrate a simple chatbot

--- client/src/views/main/Support.vue
+++ client/src/views/main/Support.vue
@@ -1,10 +1,34 @@
-<template lang="">
+<template>
   <div>
-    Test
+    <df-messenger
+      intent="WELCOME"
+      chat-title="Chatbot"
+      agent-id="8c6e64b0-4819-48ca-97b6-2f7a7407be58"
+      language-code="en"
+      expand="true"
+    ></df-messenger>
   </div>
 </template>
-<script setup>
+<script>
+export default {
+  mounted () {
+    const dialogflowScript = document.createElement('script')
+    dialogflowScript.setAttribute('src', 'https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1')
+    document.head.appendChild(dialogflowScript)
 
+    // // Initialize Dialogflow Messenger
+    // window.dfMessenger.init({
+    //   dfConfig: {
+    //     agentId: 'YOUR_AGENT_ID',
+    //     languageCode: 'en',
+    //     backgroundColor: '#FFFFFF'
+    //   },
+    //   onChatLoaded: function (chat) {
+    //     chat.open()
+    //   }
+    // })
+  }
+}
 </script>
 <style lang="">
 
